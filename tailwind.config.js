1c62585d65b6fec17515c04b0a60f3bf91059886
Stiven Castillo <stivencastillo.90@gmail.com>
2021-04-23 12:40:37 -0500

    Project created

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#FE3254',
+        'secondary': '#3396FF',
+        'orange': '#FF6851',
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'Roboto', 'sans-serif']
+      },
+      // screens: {
+      //   'mediano': {
+      //     min: '860px', max: '870px'
+      //   },
+      // },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
